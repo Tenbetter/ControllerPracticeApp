31bf50de626d0eab8bd8a63d2d619ffbff53f544
Edward <mullan.edward@gmail.com>
2020-01-27 21:48:17 +0000

    Add project files.

---
+++ ControllerPracticeApp/Controllers/PracticeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerPracticeApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PracticeController : ControllerBase
+    {
+        [Route("Ping")]
+        public string Ping()
+        {
+            return "pong";
+        }
+
+         [Route("About")]
+        public string About()
+        {
+            return "About stuff";
+        }
+
+
+    }
+}
